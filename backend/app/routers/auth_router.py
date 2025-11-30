8f6e49bb5a909272541b72224a4ccd63479dbb18
SameerRaza-2003 <sameer.raza@live.com>
2025-11-30 22:24:50 +0500

    changed register fields and login fields return

--- backend/app/routers/auth_router.py
+++ backend/app/routers/auth_router.py
@@ -1,12 +1,19 @@
+# app/routers/auth_router.py
 from fastapi import APIRouter
 from app.schemas.user_schema import UserRegister, UserLogin, UserResponse
 from app.services.auth_service import register_user, login_user
 
-router = APIRouter(prefix="/auth", tags=["Auth"])
+router = APIRouter(prefix="/auth", tags=["Auth"])  # <-- Must be named router
 
 @router.post("/register")
 def register(body: UserRegister):
-    return register_user(body.email, body.password)
+    return register_user(
+        email=body.email,
+        password=body.password,
+        name=body.name,
+        company_name=body.company_name,
+        phone=body.phone
+    )
 
 @router.post("/login", response_model=UserResponse)
 def login(body: UserLogin):
